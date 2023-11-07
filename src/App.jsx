842e441116f8353a2fb473001a3a9362ba9fd6e7
Charless404 <aragonaugusto588@gmail.com>
2023-11-06 18:04:05 -0600

    Add tailwindcss and create cv content container

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './styles/App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const sidebarBtns = [
+  { name: 'Design', icon: 'fi fi-sr-layout-fluid' },
+  { name: 'Elements', icon: 'fi fi-sr-apps-delete' },
+  { name: 'Text', icon: 'fi fi-sr-text' }
+]
 
+export default function App() {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="flex">
+        <ul
+          className="
+            bg-[#242424]
+            text-[#efefef] 
+            h-screen 
+            flex 
+            flex-col 
+            gap-2 
+            p-[1rem] 
+            px-[0.5rem]"
+        >
+          {sidebarBtns.map((btn) => {
+            return (
+              <li
+                key={btn.name}
+                className="
+                  p-[0.5rem]
+                  text-center
+                  cursor-pointer
+                  [&>i]:grid
+                  "
+              >
+                <i className={btn.icon}></i>
+                {btn.name}
+              </li>
+            )
+          })}
+        </ul>
+
+        <div className="w-full h-screen p-[5em] overflow-auto">
+          <div className="bg-white w-[210mm] h-[297mm] mx-auto shadow-lg "></div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
-
-export default App
