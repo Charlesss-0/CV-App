80e607d75aef900ecb4226f61cf8677a10bf89e1
Charless404 <aragonaugusto588@gmail.com>
2023-11-14 19:45:57 -0600

    Refactor App component

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,24 @@ import { useState } from 'react'
 
 export default function App() {
   // State variables for user input
-  const [firstName, setFirstName] = useState('')
-  const [lastName, setLastName] = useState('')
-  const [jobTitle, setJobTitle] = useState('')
-  const [profileDesc, setProfileDesc] = useState('')
-  const [email, setEmail] = useState('')
-  const [phone, setPhone] = useState('')
-  const [country, setCountry] = useState('')
-  const [city, setCity] = useState('')
+  const [userInput, setUserInput] = useState({
+    firstName: '',
+    lastName: '',
+    jobTitle: '',
+    profileDesc: '',
+    email: '',
+    phone: '',
+    country: '',
+    city: ''
+  })
+
+  const handleChange = (key, e) => {
+    setUserInput((input) => ({
+      ...input,
+      [key]: e.target.value
+    }))
+  }
+
   const [prevJobPosition, setPrevJobPosition] = useState([])
   const [prevJobName, setPrevJobName] = useState([])
   const [startDate, setStartDate] = useState([])
@@ -24,6 +34,10 @@ export default function App() {
   const [trainingStart, setTrainingStart] = useState([])
   const [trainingEnd, setTrainingEnd] = useState([])
   const [trainingDescription, setTrainingDescription] = useState([])
+
+  const handlePrev = (setEvent, arr, e) => {
+    setEvent([...arr, e.target.value])
+  }
 
   const prevPositions = prevJobPosition.map((position, index) => {
     return {
@@ -45,32 +59,66 @@ export default function App() {
     }
   })
 
+  const mapJobHistory = (jobs) => {
+    return jobs.map((job, index) => (
+      <div key={index}>
+        {!job.position && !job.employer ? (
+          ''
+        ) : (
+          <>
+            {job.employer && job.position && (
+              <p>
+                {job.position} at {job.employer} <br />
+                <span>
+                  {job.start && job.end ? `${job.start} to ${job.end}` : ''}
+                </span>
+              </p>
+            )}
+            <p>{job.description}</p>
+          </>
+        )}
+      </div>
+    ))
+  }
+
+  const mapTrainingItem = (trainings) => {
+    return trainings.map((training, index) => (
+      <div key={index}>
+        {training.qualification && training.entity && (
+          <>
+            <p>
+              {training.qualification} in {training.entity} <br />
+              <span>
+                {training.start && training.end
+                  ? `${training.start} to ${training.end}`
+                  : ''}
+              </span>
+            </p>
+            <p>{training.description}</p>
+          </>
+        )}
+      </div>
+    ))
+  }
+
   const cityCountry =
-    city !== '' && country !== ''
-      ? `${city}, ${country}`
-      : city !== '' || country !== ''
-      ? `${city} ${country}`
+    userInput.city !== '' && userInput.country !== ''
+      ? `${userInput.city}, ${userInput.country}`
+      : userInput.city !== '' || userInput.country !== ''
+      ? `${userInput.city} ${userInput.country}`
       : ''
-
-  const handleEvent = (setEvent, e) => {
-    setEvent(e.target.value)
-  }
-
-  const handlePrev = (setEvent, arr, e) => {
-    setEvent([...arr, e.target.value])
-  }
 
   return (
     <main className="flex">
       <Form
-        jobTitle={(e) => handleEvent(setJobTitle, e)}
-        firstName={(e) => handleEvent(setFirstName, e)}
-        lastName={(e) => handleEvent(setLastName, e)}
-        email={(e) => handleEvent(setEmail, e)}
-        phone={(e) => handleEvent(setPhone, e)}
-        city={(e) => handleEvent(setCity, e)}
-        country={(e) => handleEvent(setCountry, e)}
-        profileDesc={(e) => handleEvent(setProfileDesc, e)}
+        jobTitle={(e) => handleChange('jobTitle', e)}
+        firstName={(e) => handleChange('firstName', e)}
+        lastName={(e) => handleChange('lastName', e)}
+        email={(e) => handleChange('email', e)}
+        phone={(e) => handleChange('phone', e)}
+        city={(e) => handleChange('city', e)}
+        country={(e) => handleChange('country', e)}
+        profileDesc={(e) => handleChange('profileDesc', e)}
         prevJobTitle={(e) => handlePrev(setPrevJobPosition, prevJobPosition, e)}
         prevEmployer={(e) => handlePrev(setPrevJobName, prevJobName, e)}
         startDate={(e) => handlePrev(setStartDate, startDate, e)}
@@ -88,51 +136,15 @@ export default function App() {
       />
 
       <ResumeTemplate
-        firstName={firstName}
-        lastName={lastName}
-        jobTitle={jobTitle}
-        profileDescription={profileDesc}
-        email={email}
-        phone={phone}
+        firstName={userInput.firstName}
+        lastName={userInput.lastName}
+        jobTitle={userInput.jobTitle}
+        profileDescription={userInput.profileDesc}
+        email={userInput.email}
+        phone={userInput.phone}
         cityCountry={cityCountry}
-        jobHistory={prevPositions.map((job, index) => (
-          <div key={index}>
-            {!job.position && !job.employer ? (
-              ''
-            ) : (
-              <>
-                <p className="font-semibold text-[1.1rem]">
-                  {job.position}{' '}
-                  {job.employer && job.position ? `at ${job.employer}` : ''}
-                  <br />
-                  <span className="font-normal text-[1rem]">
-                    {job.start && job.end ? `${job.start} to ${job.end}` : ''}
-                  </span>
-                </p>
-                <p>{job.description}</p>
-              </>
-            )}
-          </div>
-        ))}
-        training={prevTraining.map((training, index) => (
-          <div key={index}>
-            <>
-              <p className="font-semibold text-[1.1rem]">
-                {training.qualification && training.entity
-                  ? `${training.qualification} in`
-                  : ''}{' '}
-                {training.entity}
-                <br />
-                <span className="font-normal text-[1rem]">
-                  {training.start && training.end
-                    ? `${training.start} to ${training.end}`
-                    : ''}
-                </span>
-              </p>
-              <p>{training.description}</p>
-            </>
-          </div>
-        ))}
+        jobHistory={mapJobHistory(prevPositions)}
+        training={mapTrainingItem(prevTraining)}
       />
     </main>
   )
